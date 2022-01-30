614cdc8546258fd27fed8b60f50d5349e961ffbc
freedomshare <60738260+freedomshare@users.noreply.github.com>
2022-01-30 08:05:45 -0500

    Update CakePrice.tsx

--- packages/pancake-uikit/src/components/CakePrice/CakePrice.tsx
+++ packages/pancake-uikit/src/components/CakePrice/CakePrice.tsx
@@ -23,10 +23,10 @@ const PriceLink = styled.a`
   }
 `;
 
-const CakePrice: React.FC<Props> = ({ cakePriceUsd, color = "textSubtle" }) => {
+const SwapcryptoPrice: React.FC<Props> = ({ swapcryptoPriceUsd, color = "textSubtle" }) => {
   return cakePriceUsd ? (
     <PriceLink
-      href="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
+      href="https://thirsty-hugle-3daa6e.netlify.app/swap?outputCurrency=0x429199e68283bb1e193d38ba8e85dfbca4a973c9"
       target="_blank"
     >
       <LogoRound width="24px" mr="8px" />
