e98f70660c03190708e21ca8d92afa6e4be1adff
freedomshare <60738260+freedomshare@users.noreply.github.com>
2022-01-30 07:44:37 -0500

    Update Logo.tsx

--- packages/pancake-uikit/src/widgets/Menu/components/Logo.tsx
+++ packages/pancake-uikit/src/widgets/Menu/components/Logo.tsx
@@ -56,11 +56,11 @@ const Logo: React.FC<Props> = ({ isDark, href }) => {
   return (
     <Flex>
       {isAbsoluteUrl ? (
-        <StyledLink as="a" href={href} aria-label="Pancake home page">
+        <StyledLink as="a" href={href} aria-label="Swap Crypto home page">
           {innerLogo}
         </StyledLink>
       ) : (
-        <StyledLink href={href} as={linkComponent} aria-label="Pancake home page">
+        <StyledLink href={href} as={linkComponent} aria-label="Swap Crypto home page">
           {innerLogo}
         </StyledLink>
       )}
